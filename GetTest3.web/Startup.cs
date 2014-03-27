709a89a959977dbc86cc865945619d1a174cf4cf
AllenDanna <ardanna@comcast.net>
2014-03-27 10:39:48 -0500

    Initial Commit

---
+++ GetTest3.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GetTest3.web.Startup))]
+namespace GetTest3.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
